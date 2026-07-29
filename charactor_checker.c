a7d1d19bc5ebd44a3fb5e557afb213151fecd396
nishamparakkal <mohammednishamp@gmail.com>
2026-07-29 05:55:05 +0530

    check the given input is number, lower or upper case, or special chara

---
+++ charactor_checker.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("enter the charactor:\n");
+	scanf("%c",&a);
+	if(a>='A' && a<='Z')
+	printf("Upper case\n");
+	else if(a>='a' && a<='z')
+	printf("Lower case\n");
+	else if(a>='0' && a<='9')
+	printf("Digits\n");
+	else
+	printf("Special character");
+return 0;
+}
